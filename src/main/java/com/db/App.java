a0c086ff825a43b732ab682021448fe4d207c882
kiran <kiran.mallepally@gmail.com>
2017-03-04 20:21:36 +0530

    added

--- src/main/java/com/db/App.java
+++ src/main/java/com/db/App.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang.time.DateFormatUtils;
 public class App {
 
         public static void main(String[] args) {
-                System.out.println("Hello World Today is " + getToday());
+                System.out.println("Hello World Today is sunday " + getToday());
         }
 
         public static String getToday() {
